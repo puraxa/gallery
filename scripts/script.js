fd14300b6f8d27330831b151c7c3b36f2982d6f6
Edin Purivatra <edinpurivatra@gmail.com>
2019-04-02 11:53:37 +0200

    fixed multiple login click generating double gallery

--- scripts/script.js
+++ scripts/script.js
@@ -38,20 +38,22 @@ function getImg () {
     if(http.readyState==4 && http.status==200){
       information = JSON.parse(http.responseText);
       console.log(information);
+      let gallery = ``;
       for(let i = 0; i < information.Contents.length; i++){
         let url = information.base_url + '/' + information.Contents[i].Key;
         let text = information.Contents[i].Key.split('-');
         console.log(text);
         let transText = upper(text);
         console.log(transText);
-        document.getElementById('gallery').innerHTML += `<div class="card bg-secondary col-xs-6 col-sm-6 col-md-4 col-lg-3">
+        gallery += `<div class="card bg-secondary col-xs-6 col-sm-6 col-md-4 col-lg-3">
         <img class="card-img-top" src="${url}" alt="${transText} image" width="200px" height="200px">
         <div class="card-body">
           <h5 class="card-title">${transText}</h5>
         </div>
         </div>`;
-        document.getElementById('gallery-title').style = "display:block";
       }
+      document.getElementById('gallery-title').style = "display:block";
+      document.getElementById('gallery').innerHTML = gallery;
     }
   }
 }
